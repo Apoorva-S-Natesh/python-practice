2677d00acda1b9cdea39a5cb222646f2797e5a2a
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-03-25 14:48:29 +0100

    constructors

--- basics/oop_in_python.py
+++ basics/oop_in_python.py
@@ -24,3 +24,50 @@ e2.play()
 3. self is like the this keyword in java - Self always holds the address of current object
 4. each object will have its own copy of instance variable
 '''
+
+#########                 Constructors                         ##########
+class Office:
+	def __init__(self, name, location): #Parameterized constructor
+		self.name=name
+		self.location=location
+	
+	def displayInfo(self):
+		print(self.name, self.location)
+
+office1 = Office("Code", "Berlin")
+office2 = Office("FinanceS hop", "London")
+office1.displayInfo()
+print(office2.name, office2.location)
+
+class Books:
+	def __init__(self): #Default Constructor
+		self.name="Python book"
+		self.Id=4534
+
+book1 = Books()
+book2 = Books()
+print(book1.name, book1.Id)
+print(book2.name, book2.Id)
+
+"""
+__new__ Method
+This method is responsible for creating a new instance of a class. It allocates memory and returns the new object. It is called before __init__.
+
+
+class ClassName:
+    def __new__(cls, parameters):
+        instance = super(ClassName, cls).__new__(cls)
+        return instance
+To learn more, please refer to “__new__ ” method
+
+__init__ Method
+This method initializes the newly created instance and is commonly used as a constructor in Python. It is called immediately after the object is created by __new__ method and is responsible for initializing attributes of the instance.
+
+Syntax:
+
+
+class ClassName:
+    def __init__(self, parameters):
+        self.attribute = value
+Note: It is called after __new__ and does not return anything (it returns None by default)
+"""
