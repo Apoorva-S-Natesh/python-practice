b6bd4175d8d3f194cbd84bd87fd60bc8c1c90069
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-02 14:18:43 +0200

    access modifiers

--- basics/accessModifiers.py
+++ basics/accessModifiers.py
@@ -1,19 +1,41 @@
 class Demo:
 		def __init__(self):
 			self.name = "Apoo"
+			self._lastname = "Natesh" # Protected variable
+			self.__middleName = "Somanahally" #pricvate vriable
+
+		def display(self):
+			print(self.name)
+			print(self._lastname)
+			print(self.__middleName)
+
 d1 = Demo()
+d1.display()
 print(d1.name) #Accessing public property outside the class - Allowed
+print(d1._lastname) # Accessing protected variable outside the class - Allowed
+#print(d1.__middleName) # Accessing private variable outside the class - Not allowed
 
 class Demo1(Demo):
 	def disp(self):
-		print(self.name) #Accessing public property inside child class - allwed
+		print(self.name) #Accessing public property inside child class - allowed
+		print(self._lastname) #Accessing protected variable inside child class - allowed
+		#print(self.__middleName) # Accessing private variable inside child class - Not allowed
 
 d2 = Demo1()
 d2.disp()
 
 class Code:
 	def display(self):
-		print(d1.name)
+		print(d1.name)  #Accessing public property inside Non-child class - allowed
+		print(d1._lastname)  #Accessing protected property inside Non-child class - allowed
+		#print(d1.__middleName) # Accessing private property inside non-shild class - Not allowed
 
 c = Code()
-c.display() #Accessing public property inside Non-child class - allowed
+c.display()
+
+
+'''
+public : When we want to access variable anywhere in the code
+protected : When we want to access variable inside the same class and inside child class
+private : when we want to access variables only inside the same class
+'''
