23bc5fe292c507e2d141b083191e485fc0193346
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-04 14:19:26 +0200

    generator

--- basics/Generator.py
+++ basics/Generator.py
@@ -1,6 +1,10 @@
 # We cannot have more than one return statement in a function
 # multiple values from a function can be returned using yield keyword
+'''A generator is a special type of function which does not return a single value, 
+instead, it returns an iterator object with a sequence of values.'''
 
+
+# Generator function, used when dealing with large amounts of data sets, that returns an iterator that produces a sequence of values when iterated over.
 def display():
 	yield 10
 	yield 30
@@ -12,3 +16,23 @@ print(res.__next__()) # 10
 print(res.__next__()) # 30
 print(res.__next__()) # 40
 #print(res.__next__()) # StopIteration
+
+
+def fibonacci_numbers(nums):
+    x, y = 0, 1
+    for _ in range(nums):
+        x, y = y, x+y
+        yield x
+
+def square(nums):
+    for num in nums:
+        yield num**2
+
+print(sum(square(fibonacci_numbers(10))))
+
+# Output: 4895
+
+'''
+When the generator function is called, it does not execute the function body immediately. 
+Instead, it returns a generator object that can be iterated over to produce the values.
+'''
