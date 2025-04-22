9a4153cccb8550ed44685ef2310b4b478e9f8be0
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-22 15:17:49 +0200

    feat: implements signIn and signUp from homepage

--- Python_project/meal_buddy/delivery/urls.py
+++ Python_project/meal_buddy/delivery/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index),
+	path('open_signin', views.open_signin, name='open_signin'),
+	path('open_signup', views.open_signup, name='open_signup')
 ]
--- Python_project/meal_buddy/delivery/views.py
+++ Python_project/meal_buddy/delivery/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'delivery/index.html')
+
+def open_signin(request):
+	return render(request, 'delivery/signin.html')
+
+def open_signup(request):
+	return render(request, 'delivery/signup.html')
