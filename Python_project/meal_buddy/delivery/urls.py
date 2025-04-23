d7d55b262ae881bde0eacb21a057f2270fc3883d
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-23 22:54:23 +0200

    feat: add signup feature, implement POST request for signup

--- Python_project/meal_buddy/delivery/urls.py
+++ Python_project/meal_buddy/delivery/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index),
 	path('open_signin', views.open_signin, name='open_signin'),
-	path('open_signup', views.open_signup, name='open_signup')
+	path('open_signup', views.open_signup, name='open_signup'),
+	path('signup', views.signup, name='signup'),
 ]
--- Python_project/meal_buddy/delivery/views.py
+++ Python_project/meal_buddy/delivery/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Customer
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,22 @@ def open_signin(request):
 
 def open_signup(request):
 	return render(request, 'delivery/signup.html')
+
+def signup(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		email = request.POST.get('email')
+		mobile = request.POST.get('mobile')
+		address = request.POST.get('address')
+
+		Customer.objects.create(
+			username = username, 
+			password = password,
+			email = email,
+			mobile = mobile,
+			address = address,
+		)
+
+	#return HttpResponse("Request for signup received")
+	return render(request, 'delivery/signin.html')
